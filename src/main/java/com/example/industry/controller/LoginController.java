23f7ab36d85bd87513b936b6cf5e80ef2deca658
LaoYeti <1427313535@qq.com>
2022-03-04 15:50:12 +0800

    前后端连接

--- src/main/java/com/example/industry/controller/LoginController.java
+++ src/main/java/com/example/industry/controller/LoginController.java
@@ -1,19 +1,40 @@
 package com.example.industry.controller;
 
 
+import com.example.industry.entity.ProductionLine;
 import com.example.industry.entity.User.user;
+import com.example.industry.service.ProductionLineService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.industry.service.userService;
 
+import java.util.List;
+
 @Controller
 public class LoginController {
 
+    @Autowired
+    ProductionLineService ProductionLineService;
+
     @RequestMapping("/hello")
-    public String test2() {
-        return "index";
+    public String test() {
+        return "ZhuangTaiCHaKan";
     }
+
+    @RequestMapping("/hello2")
+    public String test2(Model model) throws Exception{
+        List<ProductionLine> productionLines = ProductionLineService.listLines();
+//        String name = "name";
+        model.addAttribute("data", productionLines);
+        return "ZhuangTaiChaKan";
+    }
+//
+//    @RequestMapping("/hello3")
+//    public String test3(){
+//        return
+//    }
 
 }
