1978c6c81a187a966cb3be26d0dd05ea5fc26c56
Tangmin555 <tangmin_0510@163.com>
2022-03-07 16:51:11 +0800

    生产计划新增方法

--- src/main/java/com/example/industry/service/productionplanService.java
+++ src/main/java/com/example/industry/service/productionplanService.java
@@ -16,7 +16,46 @@ public interface productionplanService {
     //查询生产计划
     List<productionplan> getproplan(String planName, int modelType, Timestamp plannedstart, Timestamp plannedend, String state);
 
-    //删除生茶计划
+    //删除生产计划
     boolean deleteplan(int planNumber);
+
+    //获取生产计划操作
+    String getplanoperation(int planNumber);
+
+    //获取生产计划名称
+    String getplanname(int planNumber);
+
+    //获取生产计划状态
+    String getplanstate(int planNumber);
+
+    //获取生产计划所属设备组
+    String getplandevicegroup(int planNumber);
+
+    //获取计划产量
+    int getplanyield(int planNumber);
+
+    //获取合格产量
+    int getqualifiedyield(int planNumber);
+
+    //获取不合格产量
+    int getdisqualifiedyield(int planNumber);
+
+    //获取计划明细
+    String getplandetails(int planNumber);
+
+    //获取机种型号
+    int getplanmodeltype(int planNumber);
+
+    //获取计划开始时间
+    Timestamp getplannedstart(int planNumber);
+
+    //获取计划结束时间
+    Timestamp getplannedend(int planNumber);
+
+    //获取实际开始时间
+    Timestamp getactualstart(int planNumber);
+
+    //获取实际结束时间
+    Timestamp getactualend(int planNumber);
 
 }
