0949a28a04e13dd1974ef76a8f2b6d8ecdc39a2e
LaoYeti <1427313535@qq.com>
2022-03-07 12:52:49 +0800

    改了名称

--- src/main/java/com/example/industry/controller/toPageController.java
+++ src/main/java/com/example/industry/controller/toPageController.java
@@ -14,80 +14,110 @@ public class toPageController {
     @Autowired
     ProductionLineService ProductionLineService;
 
-    @RequestMapping("/index")
+    //主页
+    @RequestMapping("/main")
     public String index(){
         return "index";
     }
 
-    @RequestMapping("/ChanLiang")
-    public String ChanLiang(){
-        return "ChanLiang";
-    }
 
-    @RequestMapping("/ChanPin")
-    public String ChanPin(){
-        return "CHanPin";
-    }
-
-    @RequestMapping("/JiaDongLv")
-    public String JiaDongLv(){
-        return "JiaDongLv";
-    }
-
-    @RequestMapping("/kanban-baojingjilu")
-    public String kanban_baojingjilu(){
-        return "kanban-baojingjilu";
-    }
-
-    @RequestMapping("/kanban-shebeichanliangjishu")
-    public String kanban_shebeichanliangjishu(){
-        return "kanban-shebeichanliangjishu";
-    }
-
-    @RequestMapping("/kanban-shebeiguochengcanshu")
-    public String kanban_shebeiguochengcanshu(){
-        return "kanban-shebeiguochengcanshu";
-    }
-
-    @RequestMapping("/knifes")
-    public String knifes(){
-        return "knifes";
-    }
-
-    @RequestMapping("/knifes-huizong")
-    public String knifes_huizong(){
-        return "knifes-huizong";
-    }
-
-    @RequestMapping("/LiYongLv")
-    public String LiYongLv(){
-        return "LiYongLv";
-    }
-
-    @RequestMapping("/OEE")
-    public String OEE(){
-        return "OEE";
-    }
-
-    @RequestMapping("/OEE1")
-    public String OEE1(){
-        return "OEE1";
-    }
-
-    @RequestMapping("/production_line")
-    public String production_line(){
-        return "production_line";
-    }
-
-    @RequestMapping("/ZhuangTaiChaKan")
+//-----------------设备状态看板-------------------
+    //即时状态查看
+    @RequestMapping("/instance_status")
     public String ZhuangTaiChaKan(Model model){
         List<ProductionLine> productionLines = ProductionLineService.listLines();
         model.addAttribute("data", productionLines);
         return "ZhuangTaiChaKan";
     }
 
-    @RequestMapping("/ZhuantTaiFenBu")
+    //报警记录分析
+    @RequestMapping("/alarm_record")
+    public String kanban_baojingjilu(){
+        return "kanban-baojingjilu";
+    }
+
+    //设备产量计数分析
+    @RequestMapping("/count_analysis")
+    public String kanban_shebeichanliangjishu(){
+        return "kanban-shebeichanliangjishu";
+    }
+
+    //看板-设备过程参数
+    @RequestMapping("/board_deviceParameter")
+    public String kanban_shebeiguochengcanshu(){
+        return "kanban-shebeiguochengcanshu";
+    }
+//--------------------------------------------------------------------
+
+
+//----------------实时数据看板---------------------
+    //设备利用率
+    @RequestMapping("/use_ratio")
+    public String LiYongLv(){
+        return "LiYongLv";
+    }
+
+    //设备稼动率
+    @RequestMapping("/activation")
+    public String JiaDongLv(){
+        return "JiaDongLv";
+    }
+//-------------------------------------------------------
+
+
+//-----------------产量分析看板-----------------
+    //产量数据
+    @RequestMapping("/output")
+    public String ChanLiang(){
+        return "ChanLiang";
+    }
+
+    //产品信息
+    @RequestMapping("/pruduct_information")
+    public String ChanPin(){
+        return "ChanPin";
+    }
+//-------------------------------------------
+
+
+//状态分布看板
+    @RequestMapping("/status_distribute")
     public String ZhuangTaiFenBu(){
         return "ZhuangTaiFenBu";
     }
+
+
+//-----------------OEE--------------------
+    //当前设备OEE
+    @RequestMapping("/OEE")
+    public String OEE(){
+        return "OEE";
+    }
+
+    //整线OEE
+    @RequestMapping("/OEE1")
+    public String OEE1(){
+        return "OEE1";
+    }
+//---------------------------------------
+
+
+//----------------刀具分析-----------------
+    //全部刀具分析
+    @RequestMapping("/cutter")
+    public String knifes(){
+        return "knifes";
+    }
+
+    //刀具汇总查询
+    @RequestMapping("/cutter_all")
+    public String knifes_huizong(){
+        return "knifes-huizong";
+    }
+//-------------------------------------------
+
+
+
+
+
 }
