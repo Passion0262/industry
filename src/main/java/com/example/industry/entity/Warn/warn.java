db4675ae176e5185f50972d31f411f12e4dfbcbd
miaolin <675988235@qq.com>
2022-03-24 14:33:59 +0800

    拼写错误

--- src/main/java/com/example/industry/entity/Warn/warn.java
+++ src/main/java/com/example/industry/entity/Warn/warn.java
@@ -19,7 +19,7 @@ public class warn {
     private String deviceStatus;   // 设备运行的状态
     private String deviceLocation;  // 设备的所处的公司名称或者地理位置
     private int warnId;        // 报错id编号
-    private String warnInfomation;   // 报错的详细信息
+    private String warnInformation;   // 报错的详细信息
     private Timestamp warnTime;     // 报错的时间
 
 }
