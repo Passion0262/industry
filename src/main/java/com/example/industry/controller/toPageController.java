a6ef6e4f2260d0cc2df29faf1e4662ab919611b6
LaoYeti <1427313535@qq.com>
2022-04-21 11:06:46 +0800

    调整

--- src/main/java/com/example/industry/controller/toPageController.java
+++ src/main/java/com/example/industry/controller/toPageController.java
@@ -43,15 +43,15 @@ public class toPageController {
 //-----------------设备状态看板-------------------
     //即时状态查看
     @RequestMapping("/instance_status")
-    public String ZhuangTaiChaKan(Model model){
+    public String instanceStatus(Model model){
         List<Device> productionLines = DeviceService.listDevices();
         model.addAttribute("data", productionLines);
-        return "ZhuangTaiChaKan";
+        return "instanceStatus";
     }
 
     //报警记录分析
     @RequestMapping("/alarm_record")
-    public String kanban_baojingjilu(Model model){
+    public String alarmRecord(Model model){
         List<warn> warnlists = WarnService.listwarn();
 //        System.out.println("==============");
 //        System.out.println(warnlists);
@@ -171,7 +171,7 @@ public class toPageController {
 
 
 
-        return "kanban-baojingjilu";
+        return "alarmRecord";
     }
 //--------------------------------------------------------------------
 
@@ -190,9 +190,9 @@ public class toPageController {
     }
 
     //设备过程参数
-    @RequestMapping("/board_deviceParameter")
-    public String kanban_shebeiguochengcanshu(){
-        return "kanban-shebeiguochengcanshu";
+    @RequestMapping("/device_parameter")
+    public String deviceParameter(){
+        return "deviceParameter";
     }
 //-------------------------------------------------------
 
@@ -232,16 +232,16 @@ public class toPageController {
 
     //设备产量计数分析
     @RequestMapping("/count_analysis")
-    public String kanban_shebeichanliangjishu(){
-        return "kanban-shebeichanliangjishu";
+    public String outputAnalysis(){
+        return "outputAnalysis";
     }
 //-------------------------------------------
 
 
 //状态分布看板
     @RequestMapping("/status_distribute")
-    public String ZhuangTaiFenBu(){
-        return "ZhuangTaiFenBu";
+    public String statusDistribute(){
+        return "statusDistribute";
     }
 
 
@@ -263,10 +263,10 @@ public class toPageController {
 //----------------刀具分析-----------------
     //全部刀具分析
     @RequestMapping("/cutter")
-    public String knifes(Model model) {
+    public String cutter(Model model) {
         List<cutter> cutterList = cutterService.listCutter();
         model.addAttribute("cutterList", cutterList);
-        return "knifes";
+        return "cutter";
     }
 
     //刀具汇总查询
