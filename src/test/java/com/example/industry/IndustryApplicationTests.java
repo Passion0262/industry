30aaab3c55c41aeba8b9e5f1ed74f678be2d9351
aSir <2458990715@qq.com>
2022-06-21 16:00:37 +0800

    云端数据展示页面更改（data_index.html进入）

--- src/test/java/com/example/industry/IndustryApplicationTests.java
+++ src/test/java/com/example/industry/IndustryApplicationTests.java
@@ -1,7 +1,7 @@
 package com.example.industry;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class IndustryApplicationTests {
